e0159c33473eddaa09088707694c38bebfb1640f
Shivam Singh <shivamss17.98@gmail.com>
2024-12-09 22:44:18 +0000

    Setup changes

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ app.use(cors());
 
 // connectDB
 const connectDB = require("./db/connect");
-const authenticatedUser = require("./middleware/authentication");
 
 //routers
 const userRouter = require("./routes/user");
 const jobsRouter = require("./routes/jobs");
 
+//authentication middleware
+const authenticatedUser = require("./middleware/authentication");
 // error handler
 const notFoundMiddleware = require("./middleware/notfound");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -28,15 +29,15 @@ app.use("/api/v1/jobs", authenticatedUser, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 const port = process.env.PORT || 3000;
 
